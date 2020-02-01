095a57cc7f986cf7576627ec8c714daf633c0469
sandeepcse2004 <58394871+sandeepcse2004@users.noreply.github.com>
2020-02-01 19:43:53 +0530

    added template files

--- Sandeep_Blog_App/myblog/views.py
+++ Sandeep_Blog_App/myblog/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>HOME</h1>')
+    return render(request,'home.html')
 
 def about(request):
-    return HttpResponse('<h1>ABOUT</h1>')
+    return render(request,'about.html')
